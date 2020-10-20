3cc26944571a2a498d85adb8c7b79b821b7e9768
eriBloo <19932449+EriBloo@users.noreply.github.com>
2020-10-20 13:55:46 +0200

    Updates

---
+++ precache-manifest.0bed90f35dddc5e468f976339144436d.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe5ed277ff574e904a3f91f277b97ac1",
+    "url": "/Memory-card/index.html"
+  },
+  {
+    "revision": "2623e84514d6592136d1",
+    "url": "/Memory-card/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "3669ea102cc54de83705",
+    "url": "/Memory-card/static/css/main.6a69d6fd.chunk.css"
+  },
+  {
+    "revision": "2623e84514d6592136d1",
+    "url": "/Memory-card/static/js/2.3e9f7d4f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Memory-card/static/js/2.3e9f7d4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3669ea102cc54de83705",
+    "url": "/Memory-card/static/js/main.67143f6f.chunk.js"
+  },
+  {
+    "revision": "9b93203549398015ebeb",
+    "url": "/Memory-card/static/js/runtime-main.3d0e73b0.js"
+  },
+  {
+    "revision": "1740b92002936bb59dcecf764a9badcc",
+    "url": "/Memory-card/static/media/Abra.1740b920.png"
+  },
+  {
+    "revision": "2d2a5d525733959f2315c6a24b6aad15",
+    "url": "/Memory-card/static/media/Bulbasaur.2d2a5d52.png"
+  },
+  {
+    "revision": "c137ec0d43a67828128221911a58a9f8",
+    "url": "/Memory-card/static/media/Charizard.c137ec0d.png"
+  },
+  {
+    "revision": "b8698b476ac9036c8f5b546c6a79d501",
+    "url": "/Memory-card/static/media/Charmander.b8698b47.png"
+  },
+  {
+    "revision": "ae97d066735141a549618b21df348620",
+    "url": "/Memory-card/static/media/Charmeleon.ae97d066.png"
+  },
+  {
+    "revision": "9d48c6a63bf3db4591e9c77c0ba3a5b2",
+    "url": "/Memory-card/static/media/Diglett.9d48c6a6.png"
+  },
+  {
+    "revision": "675ac9037b8e948a338b4f8f79531a78",
+    "url": "/Memory-card/static/media/Doduo.675ac903.png"
+  },
+  {
+    "revision": "649c697c515275b6f48dcf76171ad514",
+    "url": "/Memory-card/static/media/Dugtrio.649c697c.png"
+  },
+  {
+    "revision": "0d04c313cc70df2447e9e3016e58b6ed",
+    "url": "/Memory-card/static/media/Eevee.0d04c313.png"
+  },
+  {
+    "revision": "1fd941350b3f78e36dea0d055681c1f9",
+    "url": "/Memory-card/static/media/Ekans.1fd94135.png"
+  },
+  {
+    "revision": "4f6b5063ba7d13af3c51c0efca905a0f",
+    "url": "/Memory-card/static/media/Flareon.4f6b5063.png"
+  },
+  {
+    "revision": "94877d7fcb514e5770a7e24ef3e0d8de",
+    "url": "/Memory-card/static/media/Gastly.94877d7f.png"
+  },
+  {
+    "revision": "11e9866d724f9521087d47e1cde8a785",
+    "url": "/Memory-card/static/media/Geodude.11e9866d.png"
+  },
+  {
+    "revision": "7bc136a79f64de854bf8fe2711ec0115",
+    "url": "/Memory-card/static/media/Golbat.7bc136a7.png"
+  },
+  {
+    "revision": "dcf25959e2fe6a40ab96570d65bbcfae",
+    "url": "/Memory-card/static/media/Ivysaur.dcf25959.png"
+  },
+  {
+    "revision": "3149131d03082fddb8b096d9c3aeff48",
+    "url": "/Memory-card/static/media/Jigglypuff.3149131d.png"
+  },
+  {
+    "revision": "05eec118982aef5003e599d99f546f74",
+    "url": "/Memory-card/static/media/Jolteon.05eec118.png"
+  },
+  {
+    "revision": "2460a5130645b002cbde3bf69e3a5199",
+    "url": "/Memory-card/static/media/Kadabra.2460a513.png"
+  },
+  {
+    "revision": "55a4b44f0b012ca94ce20c0784f46438",
+    "url": "/Memory-card/static/media/Machoke.55a4b44f.png"
+  },
+  {
+    "revision": "82c926aea33c38c7a80651bd205d345c",
+    "url": "/Memory-card/static/media/Machop.82c926ae.png"
+  },
+  {
+    "revision": "022a5efb608af49b609715d655b5245d",
+    "url": "/Memory-card/static/media/Magikarp.022a5efb.png"
+  },
+  {
+    "revision": "323fd00cfe3eed5bf8c82242c46be27c",
+    "url": "/Memory-card/static/media/Magnemite.323fd00c.png"
+  },
+  {
+    "revision": "aceae8866523895658eb10ffd7820f1e",
+    "url": "/Memory-card/static/media/Meowth.aceae886.png"
+  },
+  {
+    "revision": "03f98b6f0eee745ba07ea935118f794d",
+    "url": "/Memory-card/static/media/Mew.03f98b6f.png"
+  },
+  {
+    "revision": "ae3576665a6c57692a08e2c3e56e1b39",
+    "url": "/Memory-card/static/media/Mewtwo.ae357666.png"
+  },
+  {
+    "revision": "fd7f2b336670d79c8d1ca03edebe3ae5",
+    "url": "/Memory-card/static/media/Oddish.fd7f2b33.png"
+  },
+  {
+    "revision": "397fe455286a5cd7747ee01f570a2c36",
+    "url": "/Memory-card/static/media/Onix.397fe455.png"
+  },
+  {
+    "revision": "c3367e15aa7e9eb84ef11f516aea9171",
+    "url": "/Memory-card/static/media/Pidgey.c3367e15.png"
+  },
+  {
+    "revision": "6f49fa9e1b3ca97108e11ef5ffbc64a8",
+    "url": "/Memory-card/static/media/Pikachu.6f49fa9e.png"
+  },
+  {
+    "revision": "c76cc5c3dc4e66265acb8422d0435d3e",
+    "url": "/Memory-card/static/media/Psyduck.c76cc5c3.png"
+  },
+  {
+    "revision": "eed3c0c85cbc8aef2e65f3ccd5f530ce",
+    "url": "/Memory-card/static/media/Rattata.eed3c0c8.png"
+  },
+  {
+    "revision": "c37e04044d6bcb26f2dccae0e38ec253",
+    "url": "/Memory-card/static/media/Slowpoke.c37e0404.png"
+  },
+  {
+    "revision": "e9e84cf7d0eec0f3f18564cff131a6f4",
+    "url": "/Memory-card/static/media/Snorlax.e9e84cf7.png"
+  },
+  {
+    "revision": "4003ba6f26c09a8bb4eea41a40f072ba",
+    "url": "/Memory-card/static/media/Squirtle.4003ba6f.png"
+  },
+  {
+    "revision": "479b00e906f34aaf729c2abaa1821b9a",
+    "url": "/Memory-card/static/media/Voltorb.479b00e9.png"
+  },
+  {
+    "revision": "ec49ea5a0fdeca5e32698b1d91774b6a",
+    "url": "/Memory-card/static/media/Vulpix.ec49ea5a.png"
+  },
+  {
+    "revision": "700d85689938e0a379c4dd46a73472bf",
+    "url": "/Memory-card/static/media/Wigglytuff.700d8568.png"
+  }
+]);
